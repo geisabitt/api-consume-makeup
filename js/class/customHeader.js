8ada9e0c024a64bf484c8a0b009e19eb74354d42
geisabitt <ageisislane@gmail.com>
2023-12-01 15:34:41 -0300

    feat(menu): create CustomHeader Tag

---
+++ js/class/customHeader.js
@@ -0,0 +1,26 @@
+class CustomHeader extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `<div class="container-fluid bg-primary">
+      <nav class="container">
+        <div class="menu"><i class="fa-solid fa-bars"></i></div>
+        <div>Loja</div>
+        <form id="search">
+          <input type="text" />
+          <button type="button">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+        <div class="favorite-menu">
+          <i class="fa-solid fa-heart"></i>Favorito
+        </div>
+        <div class="cart">
+          <span><i class="fa-solid fa-bag-shopping"></i></span>
+          Sacola
+        </div>
+        <div class="avatar"><i class="fa-solid fa-user"></i></div>
+      </nav>
+    </div>`;
+  }
+}
+customElements.define("custom-header", CustomHeader);
