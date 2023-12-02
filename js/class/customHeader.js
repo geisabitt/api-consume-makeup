5081361cbf10bd6060b0786e394c2bf5857d2c62
geisabitt <ageisislane@gmail.com>
2023-12-02 15:27:08 -0300

    chore(header): refactor customHeader

--- js/class/customHeader.js
+++ js/class/customHeader.js
@@ -2,25 +2,26 @@ class CustomHeader extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `<div class="container-fluid bg-primary">
-      <nav class="container">
-        <div class="menu"><i class="fa-solid fa-bars"></i></div>
-        <div>Loja</div>
-        <form id="search">
-          <input type="text" />
-          <button type="button">
-            <i class="fa-solid fa-magnifying-glass"></i>
-          </button>
-        </form>
-        <div class="favorite-menu">
-        <i class="fa-solid fa-heart-circle-check"></i>Favorito
-        </div>
-        <div class="cart">
-          <span><i class="fa-solid fa-bag-shopping"></i></span>
-          Sacola
-        </div>
-        <div class="avatar"><i class="fa-solid fa-user"></i></div>
-      </nav>
-    </div>`;
+    <nav class="container">
+      <div class="menu"><i class="fa-solid fa-bars"></i></div>
+      <div>Loja</div>
+      <form id="search">
+        <input type="text" />
+        <button type="button">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
+      <div data-favorite-menu class="favorite-menu">
+        <span><i class="fa-solid fa-heart-circle-check"></i>Favorito</span>
+        <ul class="favorite-dropdown"></ul>
+      </div>
+      <div class="cart">
+        <span><i class="fa-solid fa-bag-shopping"></i></span>
+        Sacola
+      </div>
+      <div class="avatar"><i class="fa-solid fa-user"></i></div>
+    </nav>
+  </div>`;
   }
 }
 customElements.define("custom-header", CustomHeader);
